dfaa9243a8cef2e8fbe6856b512b1a86953642a2
Lomztein <lomztein2@gmail.com>
2015-04-22 06:48:40 +0200

    ALPHA 1.0.3_1 - Interactable Research Hotfix

--- Assets/Editor/ResearchEditor.cs
+++ Assets/Editor/ResearchEditor.cs
@@ -59,104 +59,107 @@ public class ResearchEditor : EditorWindow {
 	}
 	
 	void OnGUI () {
-		if (!research)
-			research = GameObject.Find ("ResearchMenu").GetComponent<ResearchMenu>();
+		if (!research) {
+			GameObject r = GameObject.Find ("ResearchMenu");
+			if (r) research = r.GetComponent<ResearchMenu>();
+		}else{
 
-		if (state == WindowState.All) {
-			if (action != Action.MovingResearch) {
+			if (state == WindowState.All) {
+				if (action != Action.MovingResearch) {
 
-				offset.y = GUI.VerticalSlider (new Rect (position.width - 15, 5, 10, position.height - 10), offset.y, position.height, 0);
-				if (GUI.Button (new Rect (position.width / 3, position.height - 25, position.width / 3, 20), "Add Research")) {
-					research.research.Add (Research.CreateInstance<Research>());
-					SelectResearch (research.research[research.research.Count-1]);
-					state = WindowState.All;
-					action = Action.MovingResearch;
-				}
-				
-				for (int i = 0; i < research.research.Count; i++) {
+					offset.y = GUI.VerticalSlider (new Rect (position.width - 15, 5, 10, position.height - 10), offset.y, position.height, 0);
+					if (GUI.Button (new Rect (position.width / 3, position.height - 25, position.width / 3, 20), "Add Research")) {
+						research.research.Add (Research.CreateInstance<Research>());
+						SelectResearch (research.research[research.research.Count-1]);
+						state = WindowState.All;
+						action = Action.MovingResearch;
+					}
 					
-					Research u = research.research[i];
-					Vector3 pos = research.GetPos (u);
+					for (int i = 0; i < research.research.Count; i++) {
+						
+						Research u = research.research[i];
+						Vector3 pos = research.GetPos (u);
 
-					DrawResearchButton (pos, u);
+						DrawResearchButton (pos, u);
+					}
+
+					for (int i = (int)(offset.y / 30); i < (int)(position.height / 30) + (int)(offset.y / 30); i++) {
+						GUI.Label (new Rect (10, i * 30 + 10 - offset.y, 20, 20), i.ToString ()); 
+					}
 				}
 
-				for (int i = (int)(offset.y / 30); i < (int)(position.height / 30) + (int)(offset.y / 30); i++) {
-					GUI.Label (new Rect (10, i * 30 + 10 - offset.y, 20, 20), i.ToString ()); 
-				}
-			}
+				if (action == Action.MovingResearch) {
 
-			if (action == Action.MovingResearch) {
+					// Dis part gunna be heavy and hacky.
+					// I'm truely sorry you have to see this.
 
-				// Dis part gunna be heavy and hacky.
-				// I'm truely sorry you have to see this.
+					// Create and populate unfound research.
+					List<Research> unfound = new List<Research>();
+					foreach (Research r in research.research) {
+						unfound.Add (r);
+					}
 
-				// Create and populate unfound research.
-				List<Research> unfound = new List<Research>();
-				foreach (Research r in research.research) {
-					unfound.Add (r);
-				}
+					// Foreach loops are major simple to work with, yay! :D
 
-				// Foreach loops are major simple to work with, yay! :D
+					for (int y = (int)(offset.y / 30); y < (int)(position.height / 30) + (int)(offset.y / 30); y++) {
+						for (int x = (int)((offset.x - position.width/2) / 30); x < (int)((position.width/2) / 30); x++) {
 
-				for (int y = (int)(offset.y / 30); y < (int)(position.height / 30) + (int)(offset.y / 30); y++) {
-					for (int x = (int)((offset.x - position.width/2) / 30); x < (int)((position.width/2) / 30); x++) {
-
-						// Figure out if there is research at current position, search in unfound.
-						Research atPos = null;
-						foreach (Research r in unfound) {
-							if (r.x == x && r.y == y) {
-								atPos = r;
+							// Figure out if there is research at current position, search in unfound.
+							Research atPos = null;
+							foreach (Research r in unfound) {
+								if (r.x == x && r.y == y) {
+									atPos = r;
+								}
 							}
-						}
 
-						if (atPos != null) {
-							DrawResearchButton (new Vector3 (x,y), atPos);
-							unfound.Remove (atPos);
-						}else{
-							DrawResearchButton (new Vector3 (x,y), null);
+							if (atPos != null) {
+								DrawResearchButton (new Vector3 (x,y), atPos);
+								unfound.Remove (atPos);
+							}else{
+								DrawResearchButton (new Vector3 (x,y), null);
+							}
 						}
 					}
 				}
+					
 			}
-				
-		}
 
-		if (state == WindowState.Research) {
-			focusResearch.name = EditorGUILayout.TextField ("Name: ",focusResearch.name);
-			focusResearch.desc = EditorGUILayout.TextArea (focusResearch.desc);
-			focusResearch.func = EditorGUILayout.TextField ("Function: ",focusResearch.func);
-			focusResearch.cost = EditorGUILayout.IntField ("Cost: ",focusResearch.cost);
-			focusResearch.sprite = (Sprite)EditorGUILayout.ObjectField ("Sprite: ",focusResearch.sprite, typeof (Sprite), false);
-			if (focusResearch.prerequisite != null) {
-				if (GUILayout.Button ("Remove Prerequisite: " + focusResearch.prerequisite.name)) {
-					Debug.Log ("Removing Prerequisitite");
-					focusResearch.prerequisite = null;
+			if (state == WindowState.Research) {
+				focusResearch.name = EditorGUILayout.TextField ("Name: ",focusResearch.name);
+				focusResearch.desc = EditorGUILayout.TextArea (focusResearch.desc);
+				focusResearch.func = EditorGUILayout.TextField ("Function: ",focusResearch.func);
+				focusResearch.cost = EditorGUILayout.IntField ("Cost: ",focusResearch.cost);
+				focusResearch.sprite = (Sprite)EditorGUILayout.ObjectField ("Sprite: ",focusResearch.sprite, typeof (Sprite), false);
+				if (focusResearch.prerequisite != null) {
+					if (GUILayout.Button ("Remove Prerequisite: " + focusResearch.prerequisite.name)) {
+						Debug.Log ("Removing Prerequisitite");
+						focusResearch.prerequisite = null;
+					}
+				}else{
+					if (GUILayout.Button ("Add Prerequisite")) {
+						state = WindowState.All;
+						action = Action.ChoosingPrerequisite;
+					}
 				}
-			}else{
-				if (GUILayout.Button ("Add Prerequisite")) {
+
+				if (GUILayout.Button ("Move")) {
 					state = WindowState.All;
-					action = Action.ChoosingPrerequisite;
+					action = Action.MovingResearch;
 				}
-			}
 
-			if (GUILayout.Button ("Move")) {
-				state = WindowState.All;
-				action = Action.MovingResearch;
-			}
+				if (GUILayout.Button ("Delete")) {
+					research.research.Remove (focusResearch);
+					DestroyImmediate (focusResearch);
+					state = WindowState.All;
+					action = Action.Default;
+					focusResearch = null;
+				}
 
-			if (GUILayout.Button ("Delete")) {
-				research.research.Remove (focusResearch);
-				DestroyImmediate (focusResearch);
-				state = WindowState.All;
-				action = Action.Default;
-				focusResearch = null;
-			}
-
-			if (GUILayout.Button ("Return")) {
-				state = WindowState.All;
-				action = Action.Default;
-				focusResearch = null;
+				if (GUILayout.Button ("Return")) {
+					state = WindowState.All;
+					action = Action.Default;
+					focusResearch = null;
+				}
 			}
 		}
 	}
